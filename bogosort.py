739c97ffc1472a5eb22cecb2c88f047fabe6c06c
Jadsabbagh <164062104+Jadsabbagh@users.noreply.github.com>
2024-03-26 22:04:53 +0300

    Update bogosort.py

--- bogosort.py
+++ bogosort.py
@@ -5,9 +5,10 @@ def Bogosort(original, shuffled):
     return shuffled
 
 #example use:
-Sorted_list = [0, 1, 2, 3, 4, 5]
-shuffling_list = Sorted_list.copy()
-random.shuffle(shuffling_list)
-
-Bogosort(Sorted_list,shuffling_list)
-print(shuffling_list)
+if __name__ == '__main__':
+    Sorted_list = [0, 1, 2, 3, 4, 5]
+    shuffling_list = Sorted_list.copy()
+    random.shuffle(shuffling_list)
+    
+    Bogosort(Sorted_list,shuffling_list)
+    print(shuffling_list)
